41e8a489152a6ee1a12488d8a5d02bd79695d06e
Eurico Catumbela <erickjeremmy@gmail.com>
2020-06-20 22:10:01 +0200

    Joshua Website

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+//Staatliches
+app.use(express.static("css"));
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+// app.get('/',(req,res) => {
+//   res.render('Contact');
+// });
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
